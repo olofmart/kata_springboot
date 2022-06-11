9e086d3cce00f7e8133f57db5ff0412a14c61422
martynovov@yahoo.com <martynovov@yahoo.com>
2022-06-11 15:59:11 +0300

    initial commit

---
+++ src/test/java/ru/olmart/kata_springboot/KataSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.olmart.kata_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
